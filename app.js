5534da4785bb5707fa07abc28726acca917a5690
abdulvokhidjon <162565383+abdulvokhidjon@users.noreply.github.com>
2024-04-22 11:44:40 +0500

    added

--- app.js
+++ app.js
@@ -28,3 +28,435 @@ if (n > 0) {
 
 //  if (n<0) {
 //   alert(n-2);}
+
+// N {4}
+// let num1 = Number(prompt("Marhamat qilib son kiriting"));
+// let num2 = Number(prompt("Marhamat qilib son kiriting"));
+// let num3 = Number(prompt("Son kiriting"));
+// let sum = 0;
+
+// if (num1 > 0) {
+//   sum++;
+// }
+// if (num2 > 0) {
+//   sum++;
+// }
+// if (num3 > 0) {
+//   sum++;
+// }
+// alert(`${sum} ta`);
+
+// N{5}
+
+// let num1 = Number(prompt("Marhamat qilib son kiriting"));
+// let num2 = Number(prompt("Marhamat qilib son kiriting"));
+// let num3 = Number(prompt("Marhamat qilib son kiriting"));
+// let sum = 0;
+// let sumNeg = 0;
+// if (num1 > 0) {
+//   sum++;
+// } else {
+//   sumNeg++;
+// }
+// if (num2 > 0) {
+//   sum++;
+// } else {
+//   sumNeg++;
+// }
+// if (num3 > 0) {
+//   sum++;
+// } else {
+//   sumNeg++;
+// }
+// alert(`${sum} ta musbat, ${sumNeg} ta manfiy`);
+
+// N{6}
+
+// let num1 = Number(prompt("Marhamat qilib son kiriting"));
+// let num2 = Number(prompt("Marhamat qilib son kiriting"));
+
+// if (num1 > num2) {
+//   alert(`${num1} katta `);
+// } else {
+//   alert(`${num2} katta `);
+// }
+
+// N{7}
+
+// let num1 = Number(prompt("Marhamat qilib son kiriting"));
+// let num2 = Number(prompt("Marhamat qilib son kiriting"));
+
+// if (num1 > num2) {
+//   alert(`2-kichik `);
+// } else {
+//   alert(`1-kichik `);
+// }
+
+// N {8}
+
+// let num1 = Number(prompt("Marhamat qilib son kiriting"));
+// let num2 = Number(prompt("Marhamat qilib son kiriting"));
+
+// if (num1 > num2) {
+//   alert(`${num1}, ${num2} `);
+// } else {
+//   alert(`${num2}, ${num1} `);
+// }
+
+// N {9}
+
+// let num1 = Number(prompt("Marhamat qilib son kiriting"));
+// let num2 = Number(prompt("Marhamat qilib son kiriting"));
+
+// if (num1 > num2) {
+//   alert(`${num2}, ${num1} `);
+// } else {
+//   alert(`${num1}, ${num2} `);
+// }
+
+// N{10}
+
+// let num1 = Number(prompt("Son kiriting"));
+// let num2 = Number(prompt("Son kiriting"));
+
+// if (num1 != num2) {
+//   alert(`a=${num1 + num2}, b=${num1 + num2}`);
+// } else {
+//   alert(`0 , 0 `);
+// }
+
+// N{11}
+
+// let num1 = Number(prompt("Son kiriting"));
+// let num2 = Number(prompt("Son kiriting"));
+
+// if (num1 > num2) {
+//   alert(`${num1}, ${num1}`);
+// } else if (num2 > num1) {
+//   alert(`${num2}, ${num2}`);
+// } else {
+//   alert(`0, 0`);
+// }
+
+// N{12}
+// let num1 = Number(prompt("Son kiriting"));
+// let num2 = Number(prompt("Son kiriting"));
+// let num3 = Number(prompt("Son kiriting"));
+
+// if (num1 <= num2 && num1 <= num3) {
+//   alert(num1);
+// } else if (num2 <= num3 && num2 <= num1) {
+//   alert(num2);
+// } else if (num3 <= num2 && num3 <= num1) {
+//   alert(num3);
+// } else {
+//   alert("hello");
+// }
+
+// N{13}
+
+// let num1 = Number(prompt("Son kiriting"));
+// let num2 = Number(prompt("Son kiriting"));
+// let num3 = Number(prompt("Son kiriting"));
+// if ((num1 <= num2 && num1 >= num3) || (num1 >= num2 && num1 <= num3)) {
+//   alert(num1);
+// } else if ((num2 <= num3 && num2 >= num1) || (num2 >= num3 && num2 <= num1)) {
+//   alert(num2);
+// } else if ((num3 <= num2 && num3 >= num1) || (num3 >= num2 && num3 <= num1)) {
+//   alert(num3);
+// } else {
+//   alert("hello");
+// }
+
+// N{14}
+
+// let num1 = Number(prompt("Son kiriting"));
+// let num2 = Number(prompt("Son kiriting"));
+// let num3 = Number(prompt("Son kiriting"));
+// if (num1 <= num2 && num1 >= num3) {
+//   alert(`${num3} ${num2}`);
+// } else if (num1 >= num2 && num1 <= num3) {
+//   alert(`${num2} ${num3}`);
+// } else if (num2 <= num3 && num2 >= num1) {
+//   alert(`${num1}, ${num3}`);
+// } else if (num2 >= num3 && num2 <= num1) {
+//   alert(`${num3}, ${num1}`);
+// } else if (num3 <= num2 && num3 >= num1) {
+//   alert(`${num2}, ${num1}`);
+// } else if (num3 >= num2 && num3 <= num1) {
+//   alert(`${num1}, ${num2}`);
+// } else {
+//   alert("hello");
+// }
+
+// N {15}
+
+// let num1 = Number(prompt("Son kiriting"));
+// let num2 = Number(prompt("Son kiriting"));
+// let num3 = Number(prompt("Son kiriting"));
+// if (num1 <= num2 && num1 >= num3) {
+//   alert(`${num1} ${num2}`);
+// } else if (num1 >= num2 && num1 <= num3) {
+//   alert(`${num1} ${num3}`);
+// } else if (num2 <= num3 && num2 >= num1) {
+//   alert(`${num2}, ${num3}`);
+// } else if (num2 >= num3 && num2 <= num1) {
+//   alert(`${num2}, ${num1}`);
+// } else if (num3 <= num2 && num3 >= num1) {
+//   alert(`${num3}, ${num1}`);
+// } else if (num3 >= num2 && num3 <= num1) {
+//   alert(`${num3}, ${num2}`);
+// } else {
+//   alert("hello");
+// }
+
+
+// Boolean N {1}
+
+
+// let num = Number(prompt("Son kiriting"));
+// if (num > 0) {
+//   console.log("Bu musbat son");
+// } else {
+//   console.log("Bu musbat son emas");
+// }
+
+N{2}
+
+// let num = Number(prompt("Son kiriting"));
+// if (num % 2 == 1) {
+//   console.log("Bu toq son");
+// } else {
+//   console.log("Bu toq son emas");
+// }
+
+N{3}
+
+// let num = Number(prompt("Son kiriting"));
+// if (num % 2 == 0) {
+//   console.log("Bu juft son");
+// } else {
+//   console.log("Bu juft son emas");
+// }
+
+N{4}
+
+// let a = Number(prompt("Son kiriting"));
+// let b = Number(prompt("Son kiriting"));
+// if (a > 2 && b <= 3) {
+//   console.log("A 2 dan katta va B 3dan kichik yoki teng");
+// } else {
+//   console.log("A 2 dan katta emas va B 3dan kichik yoki teng emas");
+// }
+
+N{5}
+
+// let a = Number(prompt("Son kiriting"));
+// let b = Number(prompt("Son kiriting"));
+// if (a >= 0 && b < -2) {
+//   console.log("A 0 dan katta yoki teng va B -2 dan kichik ");
+// } else {
+//   console.log("A 0 dan katta yoki teng emas va B -2 dan kichik emas");
+// }
+
+N{6}
+
+// let a = Number(prompt("a Son kiriting"));
+// let b = Number(prompt("b Son kiriting"));
+// let c = Number(prompt("c Son kiriting"));
+// if (a <= b && b <= c) {
+//   console.log(true );
+// } else {
+//   console.log(false);
+// }
+
+N{7}
+
+// let a = Number(prompt("a Son kiriting"));
+// let b = Number(prompt("b Son kiriting"));
+// let c = Number(prompt("c Son kiriting"));
+// if ((a < b && b < c) || (c < b && b < a)) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+N{8}
+
+// let a = Number(prompt("a Son kiriting"));
+// let b = Number(prompt("b Son kiriting"));
+// if (a % 2 == 1 && b % 2 == 1) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+/N{9}
+// let a = Number(prompt("a Son kiriting"));
+// let b = Number(prompt("b Son kiriting"));
+// if (a % 2 == 1 || b % 2 == 1) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+N{10}
+
+// let a = Number(prompt("a Son kiriting"));
+// let b = Number(prompt("b Son kiriting"));
+// if ((a % 2 == 1 && b % 2 == 0) || (a % 2 == 0 && b % 2 == 1)) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+N{11}
+
+// let a = Number(prompt("a Son kiriting"));
+// let b = Number(prompt("b Son kiriting"));
+// if ((a % 2 == 1 && b % 2 == 1) || (a % 2 == 0 && b % 2 == 0)) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+N{12}
+// let a = Number(prompt("a Son kiriting"));
+// let b = Number(prompt("b Son kiriting"));
+// let c = Number(prompt("c Son kiriting"));
+// if (a > 0 && b > 0 && c > 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+N{13}
+
+// let a = Number(prompt("a Son kiriting"));
+// let b = Number(prompt("b Son kiriting"));
+// let c = Number(prompt("c Son kiriting"));
+// if (a > 0 || b > 0 || c > 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+N{14}
+
+// let a = Number(prompt("a Son kiriting"));
+// let b = Number(prompt("b Son kiriting"));
+// let c = Number(prompt("c Son kiriting"));
+// if (
+//   (a > 0 && b < 0 && c < 0) ||
+//   (a < 0 && b > 0 && c < 0) ||
+//   (a < 0 && b < 0 && c > 0)
+// ) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+N{15}
+
+// let a = Number(prompt("a Son kiriting"));
+// let b = Number(prompt("b Son kiriting"));
+// let c = Number(prompt("c Son kiriting"));
+// if (
+//   (a > 0 && b > 0 && c < 0) ||
+//   (a < 0 && b > 0 && c > 0) ||
+//   (a > 0 && b < 0 && c > 0)
+// ) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+N{16}
+
+// let n = Number(prompt("n Son kiriting"));
+
+// if (n % 2 == 0 && n > 9 && n < 100) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+N{17}
+
+// let n = Number(prompt("n Son kiriting"));
+
+// if (n % 2 == 1 && n > 99 && n < 1000) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+N{18}
+
+// let a = Number(prompt("a Son kiriting"));
+// let b = Number(prompt("b Son kiriting"));
+// let c = Number(prompt("c Son kiriting"));
+// if (a == b || a == c || c == b) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+N{19}
+
+// let a = Number(prompt("a Son kiriting"));
+// let b = Number(prompt("b Son kiriting"));
+// let c = Number(prompt("c Son kiriting"));
+// if (a == -b || a == -c || c == -b) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+N{20}
+
+// let n = Number(prompt("3 xonali son kiriting"));
+// let a = n % 10;
+// let b = ((n % 100) - (n % 10)) / 10;
+// let c = (n - (n % 100)) / 100;
+// if (a != b && a != c && c != b) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+N{21}
+
+// let n = Number(prompt("3 xonali son kiriting"));
+// let a = n % 10;
+// let b = ((n % 100) - (n % 10)) / 10;
+// let c = (n - (n % 100)) / 100;
+// if (a > b && a > c && c < b) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+N{22}
+
+// let n = Number(prompt("3 xonali son kiriting"));
+// let a = n % 10;
+// let b = ((n % 100) - (n % 10)) / 10;
+// let c = (n - (n % 100)) / 100;
+// if ((a > b && a > c && c < b) || (a < b && a < c && c > b)) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+N{23}
+
+// let n = Number(prompt("3 xonali son kiriting"));
+// let a = n % 10;
+// let b = ((n % 100) - (n % 10)) / 10;
+// let c = (n - (n % 100)) / 100;
+// if (a == c && n > 99 && n < 1000) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
