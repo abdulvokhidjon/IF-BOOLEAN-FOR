a7e6e82c52bed1145ccdb0a428dddd4deffa2a80
abdulvokhidjon <162565383+abdulvokhidjon@users.noreply.github.com>
2024-04-22 10:54:47 +0500

    IF,BOOLEAN,FOR. started

---
+++ app.js
@@ -0,0 +1,30 @@
+// IF N{1}
+
+let n = Number(prompt("Marhamat qilib sonni kiriting."));
+if (n > 0) {
+  alert(n + 1);
+} else {
+  alert(n);
+}
+
+// N{2}
+
+// let n = Number(prompt("Marhamat qilib sonni kiriting."));
+// if (n > 0) {
+//   alert(n + 1);
+// } else {
+//   alert(n-2);
+// }
+
+// N{3}
+
+// let n = Number(prompt("Marhamat qilib sonni kiriting."));
+// if (n > 0) {
+//   alert(n + 1);
+// }
+
+// if(n==0) {
+//   alert((n+10)); }
+
+//  if (n<0) {
+//   alert(n-2);}
